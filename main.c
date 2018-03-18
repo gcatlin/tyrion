6ba639a641178b86d222c673267d1b9054c24aa3
Geoff Catlin <gcatlin@gmail.com>
2018-03-18 17:37:18 -0400

    formatting

--- main.c
+++ main.c
@@ -206,8 +206,9 @@ void next_token()
 {
     token.start = stream;
     switch (*stream) {
+        // clang-format off
         case '0': case '1': case '2': case '3': case '4': case '5': case '6':
-        case '7': case '8': case '9': {
+        case '7': case '8': case '9': { // clang-format on
             int val = 0;
             while (isdigit(*stream)) {
                 val *= 10;
@@ -217,6 +218,7 @@ void next_token()
             token.num = val;
             break;
         }
+        // clang-format off
         case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g':
         case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
         case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
@@ -225,7 +227,7 @@ void next_token()
         case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N':
         case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U':
         case 'V': case 'W': case 'X': case 'Y': case 'Z':
-        case '_': {
+        case '_': { // clang-format on
             while (isalnum(*stream) || *stream == '_') {
                 stream++;
             }
@@ -290,15 +292,13 @@ static inline bool expect_token(TokenKind kind)
         next_token();
         return true;
     }
-    fatal(
-        "expected token %s, got %s\n", token_kind_name(kind),
-        token_kind_name(token.kind));
+    fatal("expected token %s, got %s\n", token_kind_name(kind), token_kind_name(token.kind));
     return false;
 }
 
-#define assert_token(x)      assert(match_token(x))
-#define assert_token_eof()   assert(is_token(0))
-#define assert_token_int(x)  assert(token.num == (x) && match_token(TOKEN_INT))
+#define assert_token(x) assert(match_token(x))
+#define assert_token_eof() assert(is_token(0))
+#define assert_token_int(x) assert(token.num == (x) && match_token(TOKEN_INT))
 #define assert_token_name(x) assert(token.name == str_intern(x) && match_token(TOKEN_NAME))
 
 void lex_test(void)
@@ -409,6 +409,7 @@ int parse_expr_str(const char *str)
 
 void parse_test(void)
 {
+    // clang-format off
     assert_expr(1);
     assert_expr(-1);
     assert_expr(1);
